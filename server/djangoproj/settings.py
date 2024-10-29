6e1f452534c885a779f4ca1aa97af5ad4d1f4012
Omar-Elhosary <omarzaky646@gmail.com>
2024-10-29 16:54:11 -0400

    Handle models and view with Django

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,11 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     'localhost', 
-    'https://omarzaky646-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai', 
+    'https://omarzaky646-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai', 
     '127.0.0.1'
 ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://omarzaky646-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    'https://omarzaky646-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
     'http://127.0.0.1:8000'
 ]
 
